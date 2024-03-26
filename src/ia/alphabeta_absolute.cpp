9799915faa5c149337c357e3bdf6d094093c71c4
fredo <sausse.sylvain88@gmail.com>
2024-03-26 08:43:57 +0100

    deleted blank cringe space

--- src/ia/alphabeta_absolute.cpp
+++ src/ia/alphabeta_absolute.cpp
@@ -5,7 +5,7 @@ using namespace IA;
 AlphaBeta_Absolute::AlphaBeta_Absolute(int __depth): AlphaBeta(__depth) {}
 
 int AlphaBeta_Absolute::heuristics(const othello::Board& current_board, const othello::pawn team ) {
-        int result = 0;
+    int result = 0;
     for(int i=0 ; i < othello::Board::length ; i++) {
         if(current_board.getCase(i) == team) {
             result ++;
